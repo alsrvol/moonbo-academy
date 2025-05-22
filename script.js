12eacacadfffa3562dadbe8baf7a872f0895afc4
alsrvol <alsrvol@naver.com>
2025-05-22 13:47:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const menuToggle = document.querySelector('.menu-toggle')
+const sideMenu = document.getElementById('sideMenu')
+const overlay = document.getElementById('overlay')
+
+menuToggle.addEventListener('click', () => {
+  sideMenu.classList.toggle('active')
+  overlay.classList.toggle('active')
+})
+
+overlay.addEventListener('click', () => {
+  sideMenu.classList.remove('active')
+  overlay.classList.remove('active')
+})
+
 const slides = document.querySelectorAll('.slide')
 const dots = document.querySelectorAll('.dot')
 let currentIndex = 0
